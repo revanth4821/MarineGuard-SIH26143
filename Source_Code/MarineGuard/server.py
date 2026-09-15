a97c0ae0c0faa5999e4a21167fd4033641e29ddb
revanth4821 <revanth4821@gmail.com>
2026-09-16 00:26:33 +0530

    Update server.py

--- Source_Code/MarineGuard/server.py
+++ Source_Code/MarineGuard/server.py
@@ -396,5 +396,15 @@ def download_report():
 
 
 if __name__ == '__main__':
-    print('MarineGuard running at http://127.0.0.1:5000')
-    app.run(host='127.0.0.1', port=5000, debug=False, use_reloader=False)
+    import os
+
+    port = int(os.environ.get('PORT', 5000))
+
+    print(f'MarineGuard running on port {port}')
+
+    app.run(
+        host='0.0.0.0',
+        port=port,
+        debug=False,
+        use_reloader=False
+    ))
